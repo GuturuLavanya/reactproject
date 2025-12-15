04872fbbc36e7251e286ca38d7affd3bf05a6009
GuturuLavanya <lavanyaguturu173@gmail.com>
2025-12-15 12:09:41 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,14 @@ function App() {
   return (
     <>
      <div>
-        <h1 style={{color:"blue"}}>Hellow World</h1>
+        <h1 style={{color:"red"}}>Hellow World</h1>
         <img src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
-        <p>Flower image descriptions highlight vibrant colors, delicate petals, unique shapes (trumpet, cluster), symbolism (roses for love, sunflowers for longevity), and settings (gardens, fields), using adjectives like vibrant, fragrant, delicate, and exquisite to capture visual and sensory details. </p>
+        <p>Flower image descriptions highlight vibrant colors, delicate petals, unique shapes (trumpet, cluster),
+           symbolism (roses for love, sunflowers for longevity), 
+           and settings (gardens, fields), using adjectives 
+           like vibrant, fragrant, delicate, and exquisite to capture visual and sensory details.
+           react is build userinterface
+            </p>
         <button style={{border:"1px solid black"}}>click</button>
       </div>
     </>
